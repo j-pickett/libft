46ce1ec36db3c244293e8bc1d4835f770476df20
j-pickett <pj259166@gmail.com>
2018-05-08 21:04:47 -0700

    Update libft.h

--- libft.h
+++ libft.h
@@ -41,7 +41,7 @@ char				*ft_itoa(int n);
 void				*ft_memalloc(size_t size);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
-int					ft_memcmp(void *s1, void *s2, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				ft_memdel(void **ap);
 void				*ft_memmove(void *dest, void *src, size_t len);
