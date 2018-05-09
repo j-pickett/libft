6e0fd6d7d36bdb433d5a487d3c8c2fa08001da94
j-pickett <pj259166@gmail.com>
2018-05-08 21:05:11 -0700

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,21 +12,21 @@
 
 #include "libft.h"
 
-int		ft_memcmp(void *s1, void *s2, size_t n)
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*str1;
-	unsigned char	*str2;
-	size_t			i;
+	unsigned char	*c1;
+	unsigned char	*c2;
+	unsigned int	i;
 
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
 	i = 0;
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
 	while (i < n)
 	{
-		if (str1[i] != str2[i])
-			return (str1[i] - str2[i]);
+		if (c1[i] != c2[i])
+			return (c1[i] - c2[i]);
 		i++;
 	}
 	return (0);
